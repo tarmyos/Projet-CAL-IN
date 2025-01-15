b9891f8d3e7c8ba90257e06d1288cc8caf51cd8f
Wadaaaaaaaaa <144777804+Wadaaaaaaaaa@users.noreply.github.com>
2025-01-15 01:54:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, request
-import math
-import numpy as np
-from scripts.black_scholes import black_scholes_call
+from scripts.black_scholes import black_scholes_call, black_scholes_put
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', result=None)
+    return render_template('index.html', call_result=None, put_result=None)
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    # Récupération des données du formulaire
-    S = float(request.form['S'])  # Prix de l'actif sous-jacent
-    K = float(request.form['K'])  # Prix d'exercice
-    T = float(request.form['T'])  # Temps jusqu'à l'échéance
-    r = float(request.form['r'])  # Taux d'intérêt sans risque
-    sigma = float(request.form['sigma'])  # Volatilité
+    try:
+        # Récupération des données du formulaire
+        S = float(request.form['S'])
+        K = float(request.form['K'])
+        T = float(request.form['T'])
+        r = float(request.form['r'])
+        sigma = float(request.form['sigma'])
 
-    # Appeler la fonction Black-Scholes pour calculer le prix de l'option
-    option_price = black_scholes_call(S, K, T, r, sigma)
+        # Calcul des prix des options
+        call_price = black_scholes_call(S, K, T, r, sigma)
+        put_price = black_scholes_put(S, K, T, r, sigma)
 
-    # Retourner le résultat à l'utilisateur
-    return render_template('index.html', result=option_price)
+        # Retour des résultats
+        return render_template('index.html', call_result=call_price, put_result=put_price)
+
+    except ValueError:
+        return render_template('index.html', call_result="Erreur dans les entrées", put_result="Erreur dans les entrées")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
