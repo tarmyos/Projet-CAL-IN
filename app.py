79eb66f24036d4e35d4dc2434c835dd0412ae5ab
tarmyos <vfodere@gmail.com>
2025-01-16 14:39:09 +0100

    feat: reindexation + navbar pour l interface

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', call_result=None, put_result=None)
 
-@app.route('/calculate', methods=['POST'])
+@app.route('/european', methods=['GET'])
+def european():
+    return render_template('european.html')
+
+@app.route('/american', methods=['GET'])
+def american():
+    return render_template('american.html')
+
+@app.route('/bermudian', methods=['GET'])
+def bermudian():
+    return render_template('bermudian.html')
+
+@app.route('/european/calculate', methods=['POST'])
 def calculate():
     try:
         # Récupération des données du formulaire
@@ -31,10 +43,10 @@ def calculate():
         put_price, delta_put = propagation_incertitudes_put(S, K, T, r, sigma, dS, dK, dT, dr, dSigma)
 
         # Retour des résultats
-        return render_template('index.html', call_result=call_price, put_result=put_price, call_uncertainty=delta_call, put_uncertainty=delta_put)
+        return render_template('european.html', call_result=call_price, put_result=put_price, call_uncertainty=delta_call, put_uncertainty=delta_put)
 
     except ValueError:
-        return render_template('index.html', call_result="Erreur dans les entrées", put_result="Erreur dans les entrées", call_uncertainty="Erreur dans les entrées", put_uncertainty="Erreur dans les entrées")
+        return render_template('european.html', call_result="Erreur dans les entrées", put_result="Erreur dans les entrées", call_uncertainty="Erreur dans les entrées", put_uncertainty="Erreur dans les entrées")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
