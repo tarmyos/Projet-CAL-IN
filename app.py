fdfa5f38deca3e0bbe7444224e0dc83792f151c5
tarmyos <vfodere@gmail.com>
2025-01-16 18:54:17 +0100

    feat: calcul option américaine, changement de route dans app.py

--- app.py
+++ app.py
@@ -1,52 +1,72 @@
 from flask import Flask, render_template, request
 from scripts.european_black_scholes import black_scholes_call, black_scholes_put
 from scripts.european_propagation_incertitudes import propagation_incertitudes_call, propagation_incertitudes_put
-
+from scripts.american_binomial import binomial_american_call, binomial_american_put
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', call_result=None, put_result=None)
 
-@app.route('/european', methods=['GET'])
+@app.route('/european', methods=['GET', 'POST'])
 def european():
-    return render_template('european.html')
+    if request.method == 'GET':
+        return render_template('european.html')
+    else:
+        try:
+            # Récupération des données du formulaire
+            S = float(request.form['S'])
+            K = float(request.form['K'])
+            T = float(request.form['T'])
+            r = float(request.form['r'])
+            sigma = float(request.form['sigma'])
 
-@app.route('/american', methods=['GET'])
+            # Incertitudes
+            dS = float(request.form['dS'])
+            dK = float(request.form['dK'])
+            dT = float(request.form['dT'])
+            dr = float(request.form['dr'])
+            dSigma = float(request.form['dSigma'])
+
+
+            # Calcul des prix des options et de leurs incertitudes associées
+            call_price, delta_call = propagation_incertitudes_call(S, K, T, r, sigma, dS, dK, dT, dr, dSigma)
+            put_price, delta_put = propagation_incertitudes_put(S, K, T, r, sigma, dS, dK, dT, dr, dSigma)
+
+            # Retour des résultats
+            return render_template('european.html', call_result=call_price, put_result=put_price, call_uncertainty=delta_call, put_uncertainty=delta_put)
+
+        except ValueError:
+            return render_template('european.html', call_result="Erreur dans les entrées", put_result="Erreur dans les entrées", call_uncertainty="Erreur dans les entrées", put_uncertainty="Erreur dans les entrées")
+
+
+@app.route('/american', methods=['GET', 'POST'])
 def american():
-    return render_template('american.html')
+    if request.method == 'GET':
+        return render_template('american.html')
+    else:
+        try:
+            # Récupération des données du formulaire
+            S = float(request.form['S'])
+            K = float(request.form['K'])
+            T = float(request.form['T'])
+            r = float(request.form['r'])
+            sigma = float(request.form['sigma'])
+            n = int(request.form['n'])
+
+            # Calcul du prix de l'option américaine Call et Put
+            call_price = binomial_american_call(S, K, T, r, sigma, n)
+            put_price = binomial_american_put(S, K, T, r, sigma, n)
+
+            # Retour des résultats
+            return render_template('american.html', call_result=call_price, put_result=put_price)
+        
+        except ValueError:
+            return render_template('american.html', call_result="Erreur dans les entrées", put_result="Erreur dans les entrées")
 
 @app.route('/bermudan', methods=['GET'])
 def bermudian():
     return render_template('bermudan.html')
-
-@app.route('/european/calculate', methods=['POST'])
-def calculate():
-    try:
-        # Récupération des données du formulaire
-        S = float(request.form['S'])
-        K = float(request.form['K'])
-        T = float(request.form['T'])
-        r = float(request.form['r'])
-        sigma = float(request.form['sigma'])
-
-        # Incertitudes
-        dS = float(request.form['dS'])
-        dK = float(request.form['dK'])
-        dT = float(request.form['dT'])
-        dr = float(request.form['dr'])
-        dSigma = float(request.form['dSigma'])
-
-
-        # Calcul des prix des options et de leurs incertitudes associées
-        call_price, delta_call = propagation_incertitudes_call(S, K, T, r, sigma, dS, dK, dT, dr, dSigma)
-        put_price, delta_put = propagation_incertitudes_put(S, K, T, r, sigma, dS, dK, dT, dr, dSigma)
-
-        # Retour des résultats
-        return render_template('european.html', call_result=call_price, put_result=put_price, call_uncertainty=delta_call, put_uncertainty=delta_put)
-
-    except ValueError:
-        return render_template('european.html', call_result="Erreur dans les entrées", put_result="Erreur dans les entrées", call_uncertainty="Erreur dans les entrées", put_uncertainty="Erreur dans les entrées")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
